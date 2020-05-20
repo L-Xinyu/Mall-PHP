66b2e26ba95ccaa86c7b3798b5a47bb56fa1101b
XinyuLi <911410598@qq.com>
2020-05-20 22:44:22 +0200

    Add config files to connect Databse and operating Session

---
+++ backend/config.php
@@ -0,0 +1,12 @@
+<?php
+date_default_timezone_set('UTC');
+return[
+    'DB_HOST' => '127.0.0.1',
+    'DB_PORT' => '3306',
+    'DB_USER' => 'root',
+    'DB_PASS' => 'lxy0325',
+    'DB_NAME' => 'dbmall',
+    'DB_PREFIX' => 'mall_',
+    'DB_CHARSET' => 'utf8',
+];
+?>
